b35aee645730f75f2f4e30c10c952f8791228ace
Mathew <mathew.tran.dev@gmail.com>
2023-12-09 14:38:11 -0800

    add arcade rec

---
+++ Scripts/Building/RecHouse.gd
@@ -0,0 +1,13 @@
+extends "res://Scripts/Building/Building.gd"
+
+func _ready():
+	super()
+	$ActiveParticle.emitting = false
+
+
+func OnActivated():
+	$ActiveParticle.emitting = true
+
+func OnDeactivated():
+	$ActiveParticle.emitting = false
+
