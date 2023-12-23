ff1032ce7bea299bf0747d23a475a52f3bd41262
Mathew <mathew.tran.dev@gmail.com>
2023-12-22 17:20:14 -0800

    working on peeple UI

--- Scripts/UI/PeepleUI/HouseDetails.gd
+++ Scripts/UI/PeepleUI/HouseDetails.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var HouseButton : Button = $Button
+@onready var HouseButton : TextureButton = $Button
 @onready var HouseNameLabel : Label = $Label
 
 signal HouseFollowClicked
@@ -9,11 +9,7 @@ var TrackedPeeple
 func Update(trackedPeeple):
 	if trackedPeeple:
 		TrackedPeeple = trackedPeeple
-		HouseButton.visible = trackedPeeple.GetHouse() != null
 		if trackedPeeple.GetHouse():
 			HouseNameLabel.text = trackedPeeple.GetHouse().name
 		else:
 			HouseNameLabel.text = "Homeless"
-
-func _on_button_button_up():
-	emit_signal("HouseFollowClicked")
